a8168cb0af95ebaa979bbe4610d0259f6c04bcb9
mamun <aalmamun295@gmail.com>
2026-04-21 16:03:11 +0600

    update project architecture structure with ui module

---
+++ spring-lens-ui/src/main/java/com/sdlcpro/springlens/ui/SpringLensUiAutoConfiguration.java
@@ -0,0 +1,24 @@
+package com.sdlcpro.springlens.ui;
+
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@AutoConfiguration
+@ConditionalOnWebApplication(type = ConditionalOnWebApplication.Type.SERVLET)
+public class SpringLensUiAutoConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/spring-lens/ui/**")
+                .addResourceLocations("classpath:/META-INF/spring-lens/ui/")
+                .setCachePeriod(0);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/spring-lens/ui", "/spring-lens/ui/index.html");
+    }
+}
