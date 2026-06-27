0371eca605cc97179007e11149e0168501ba2395
Kibriar Hossain <shantohossain.p@gmail.com>
2026-06-27 21:33:25 +0600

    Implemented BeanDefinitionInfoPersistenceHandler Component #72

---
+++ spring-lens-storage/src/main/java/com/sdlcpro/springlens/storage/bean/definition/BeanDefinitionInfoPersistenceHandler.java
@@ -0,0 +1,31 @@
+package com.sdlcpro.springlens.storage.bean.definition;
+
+import com.sdlcpro.springlens.annotation.SpringLenInternalComponent;
+import com.sdlcpro.springlens.listener.bean.definition.BeanDefinitionInfoCollectListener;
+import com.sdlcpro.springlens.model.bean.definition.BeanDefinitionInfo;
+import com.sdlcpro.springlens.model.bean.definition.BeanDefinitionInfoRepository;
+
+/**
+ * Persistence handler that bridges metadata collection and storage for
+ * {@link BeanDefinitionInfo} instances.
+ *
+ * <p>This component listens for collected {@link BeanDefinitionInfo} events
+ * via the {@link BeanDefinitionInfoCollectListener} callback and immediately
+ * persists them using the {@link BeanDefinitionInfoRepository}.</p>
+ *
+ * @since 1.0.0
+ */
+@SpringLenInternalComponent
+public class BeanDefinitionInfoPersistenceHandler implements BeanDefinitionInfoCollectListener {
+
+    private final BeanDefinitionInfoRepository beanDefinitionInfoRepository;
+
+    public BeanDefinitionInfoPersistenceHandler(BeanDefinitionInfoRepository beanDefinitionInfoRepository) {
+        this.beanDefinitionInfoRepository = beanDefinitionInfoRepository;
+    }
+
+    @Override
+    public void onBeanDefinitionInfoCollect(BeanDefinitionInfo beanDefinitionInfo) {
+        this.beanDefinitionInfoRepository.save(beanDefinitionInfo);
+    }
+}
---
+++ spring-lens-storage/src/test/java/com/sdlcpro/springlens/storage/bean/definition/BeanDefinitionInfoPersistenceHandlerTest.java
@@ -0,0 +1,60 @@
+package com.sdlcpro.springlens.storage.bean.definition;
+
+import com.sdlcpro.springlens.model.bean.BeanRole;
+import com.sdlcpro.springlens.model.bean.definition.BeanDefinitionInfo;
+import com.sdlcpro.springlens.model.bean.definition.BeanDefinitionInfoRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("BeanDefinitionInfoPersistenceHandler Tests")
+class BeanDefinitionInfoPersistenceHandlerTest {
+
+    @Mock
+    private BeanDefinitionInfoRepository beanDefinitionInfoRepository;
+
+    @InjectMocks
+    private BeanDefinitionInfoPersistenceHandler persistenceHandler;
+
+    @Test
+    @DisplayName("should save bean definition info exactly once when onBeanDefinitionInfoCollect is called")
+    void shouldSaveBeanDefinitionInfoWhenCollected() {
+        // given
+        BeanDefinitionInfo beanDefinitionInfo = new BeanDefinitionInfo(
+                "applicationContext",
+                "myService",
+                List.of("serviceAlias"),
+                "com.example.MyService",
+                "classpath:/com/example/MyService.class",
+                "My service bean",
+                "singleton",
+                false,
+                true,
+                true,
+                BeanRole.ROLE_APPLICATION,
+                "init",
+                "destroy",
+                null,
+                null,
+                List.of("dependencyA"),
+                List.of("dependentB")
+        );
+
+        // when
+        persistenceHandler.onBeanDefinitionInfoCollect(beanDefinitionInfo);
+
+        // then
+        verify(beanDefinitionInfoRepository, times(1)).save(beanDefinitionInfo);
+        verifyNoMoreInteractions(beanDefinitionInfoRepository);
+    }
+}
